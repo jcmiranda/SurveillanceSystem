d4ad2515f0fa5574efe6b5e715d16c3cc0ff5380
Jeanette Miranda <jeanette_miranda@brown.edu>
2013-04-12 23:41:19 -0400

    Reindenting

--- take_2/SurveillanceSystem.cpp
+++ take_2/SurveillanceSystem.cpp
@@ -5,22 +5,28 @@
 
 #include "video_frame.h"
 
+// Height and width of frame in pixels
 const static int FRAME_HEIGHT = 240;
 const static int FRAME_WIDTH = 360;
+
+// Length of video frame buffer
 const static int FRAME_BUFLEN = 100;
+
+// The index to which the current frame is being written
 static int cur_frame_i = 0;
-static std::vector<cv::Mat> frame_buffer(sizeof(cv::Mat));
+static std::vector<VideoFrame_t> video_frame_buffer(sizeof(VideoFrame_t));
 
 int main(int argc, char** argv) {
-
-  VideoFrame_t video_frame;
-  video_frame.frame = cv::Mat(FRAME_HEIGHT, FRAME_WIDTH, CV_8UC1, cv::Scalar(0));
-  video_frame.timestamp = time_t();
-  for (int i = 0; i < 10; i++) {
-    time(&video_frame.timestamp);
-    std::cout << video_frame.timestamp << std::endl;
-    sleep(1);
-  }
-  return 0;
+    // Capture default webcam feed
+    cv::VideoCapture video_cap(0);
+    VideoFrame_t video_frame;
+    video_frame.frame = cv::Mat(FRAME_HEIGHT, FRAME_WIDTH, CV_8UC1, cv::Scalar(0));
+    video_frame.timestamp = time_t();
+    for (int i = 0; i < 10; i++) {
+        time(&video_frame.timestamp);
+        std::cout << video_frame.timestamp << std::endl;
+        sleep(1);
+    }
+    return 0;
 }
 
