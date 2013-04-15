e9f7e88bd93bf0b58e032a168a5621ea5e02ccdb
Jeanette Miranda <jeanette_miranda@brown.edu>
2013-04-15 00:22:23 -0400

    incrementing read lock by single frame when appropriate - still testing.

--- take_2/BgdCapturerSingle.cpp
+++ take_2/BgdCapturerSingle.cpp
@@ -11,32 +11,29 @@ bool BgdCapturerSingle::runInThread() {
         int rc2 = 0;
         bool inc_cur_frame = false;
 
-        VideoFrame_t frame_1 = _frame_buffer[_cur_frame_i];
-        VideoFrame_t frame_2 = _frame_buffer[_cur_frame_i+1];
+        VideoFrame_t& frame_1 = (*_frame_buffer)[_cur_frame_i];
+        VideoFrame_t& frame_2 = (*_frame_buffer)[_cur_frame_i+1];
 
-        if( (rc1 = pthread_rwlock_rdlock(frame_1->rw_lock)) != 0) {
+        if( (rc1 = pthread_rwlock_rdlock(frame_1.rw_lock)) != 0) {
             perror("Unable to acquire read lock in BgdCapturerSingle");
         }
         
-        if( (rc2 = pthread_rwlock_rdlock(
-                        _frame_buffer[_cur_frame_i+1].rw_lock)) != 0) {
+        if( (rc2 = pthread_rwlock_rdlock(frame_2.rw_lock)) != 0) {
             perror("Unable to acquire 2nd read lock in BgdCapturerSingle");
         }
 
-        if(difftime(_frame_buffer[_cur_frame_i+1].timestamp, // end
-                    _frame_buffer[_cur_frame_i].timestamp)
+        if(difftime(frame_1.timestamp, // end
+                    frame_2.timestamp)
                 > 0) { // beg
             inc_cur_frame = true;
             std::cout << "inc" << std::endl;
         }
 
-        if( (rc1 = pthread_rwlock_unlock(
-                        _frame_buffer[_cur_frame_i].rw_lock)) != 0) {
+        if( (rc1 = pthread_rwlock_unlock(frame_1.rw_lock)) != 0) {
             perror("Unable to release read lock in BgdCapturerSingle");
         } 
         
-        if( (rc2 = pthread_rwlock_unlock(
-                        _frame_buffer[_cur_frame_i].rw_lock)) != 0) {
+        if( (rc2 = pthread_rwlock_unlock(frame_2.rw_lock)) != 0) {
             perror("Unable to release read lock in BgdCapturerSingle");
         }
 
